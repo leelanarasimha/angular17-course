b2e465cd791ec8bf322f7331934cc0765800cf71
leelanarasimha <leela.narsimha@gmail.com>
2024-01-15 19:30:57 +0530

    create component

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import ProfileComponent from './profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ProfileComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-basics';
---
+++ angular-basics/src/app/profile.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: '[app-profile], app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+})
+export default class ProfileComponent {}
