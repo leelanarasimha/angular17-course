5cad358f859c26e28114a0fe44a321890932b71a
leelanarasimha <leela.narsimha@gmail.com>
2024-01-29 23:23:55 +0530

    ngafterviewinit ngaftercontentinit

--- angular-basics/src/app/life-cycle-hooks/life-cycle-hooks.component.ts
+++ angular-basics/src/app/life-cycle-hooks/life-cycle-hooks.component.ts
@@ -1,15 +1,29 @@
-import { Component, DestroyRef, Input, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  ContentChild,
+  DestroyRef,
+  ElementRef,
+  Input,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import ProfileComponent from '../profile.component';
 
 @Component({
   selector: 'app-life-cycle-hooks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './life-cycle-hooks.component.html',
   styleUrl: './life-cycle-hooks.component.css',
 })
 export class LifeCycleHooksComponent {
   @Input() title = { name: '' };
   @Input() counter = 0;
+  loading = true;
+  @ContentChild(ProfileComponent) profileContent!: ProfileComponent;
+
+  @ViewChild('leela') leelaElement!: ElementRef;
   name = '';
   constructor(private destroyRef: DestroyRef) {
     console.log('constructor called');
@@ -39,5 +53,15 @@ export class LifeCycleHooksComponent {
 
   ngOnDestroy() {
     console.log('component destroyed');
+  }
+
+  ngAfterContentInit() {
+    console.log(this.profileContent);
+    console.log('after contentinit fired');
+  }
+
+  ngAfterViewInit() {
+    console.log(this.leelaElement.nativeElement);
+    console.log('after view init initialized');
   }
 }
