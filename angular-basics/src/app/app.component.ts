da742eabf4b8a43a6be8060c7d2957a53a39b4cf
leelanarasimha <leela.narsimha@gmail.com>
2024-02-09 23:59:20 +0530

    view children

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import ProfileComponent from './profile.component';
@@ -11,6 +11,7 @@ import { LifeCycleHooksComponent } from './life-cycle-hooks/life-cycle-hooks.com
 import { CheckedLifeCycleComponent } from './checked-life-cycle/checked-life-cycle.component';
 import { AfterRenderComponent } from './after-render/after-render.component';
 import { ViewChildExampleComponent } from './view-child-example/view-child-example.component';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
@@ -27,19 +28,38 @@ import { ViewChildExampleComponent } from './view-child-example/view-child-examp
     LifeCycleHooksComponent,
     CheckedLifeCycleComponent,
     AfterRenderComponent,
-    ViewChildExampleComponent
+    ViewChildExampleComponent,
+    TaskComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+
+  tasks = ['task 1', 'task 2', 'task 3', 'task 4'];
   title = { name: 'angular-basics' };
   counter: number = 20;
   receivedDataFromChild = '';
   isActive = true;
 
+  @ViewChildren(TaskComponent, {read: ElementRef}) taskComponents!:
+  QueryList<ElementRef>;
+
   dataReceived(data: string) {
     this.receivedDataFromChild = data;
+  }
+
+  ngAfterViewInit() {
+    this.taskComponents.forEach(taskComponent => {
+      console.log(taskComponent);
+    });
+    this.taskComponents.changes.subscribe(() => {
+      console.log("child components has changed");
+    });
+
+    setTimeout(() => {
+      this.tasks.push("hi leela");
+    }, 3000)
   }
 
   changeTitle() {
---
+++ angular-basics/src/app/task/task.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+
+  @Input() taskName!: string;
+}
